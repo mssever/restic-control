c384a2510e89f62c847d8af719d1baea959aec1d
Scott Severance <scott@scottseverance.us>
2023-04-23 22:31:58 -0400

    Alphabetized/reorganized names

--- restic_control.py
+++ restic_control.py
@@ -13,39 +13,12 @@ import psutil
 implemented_commands = ('backup', 'prune', 'check', 'check_read')
 implemented_combo_commands = ('backup_check', 'backup_check_read')
 
-def make_backup_command():
-    try:
-        include = ['--files-from', os.environ['RESTIC_INCLUDES_FILE']]
-        if not os.path.isfile(include[1]):
-                raise FileNotFoundError('Includes file configured but not found')
-    except KeyError:
-        include = []
-    try:
-        exclude = ['--exclude-file', os.environ['RESTIC_EXCLUDES_FILE']]
-        if not os.path.isfile(exclude[1]):
-                raise FileNotFoundError('Excludes file configured but not found')
-    except KeyError:
-        exclude = []
-    return ['backup'] + include + exclude
-
-def make_prune_command():
-    return [
-        'forget',
-        '--prune',
-        '--keep-last', os.environ['RESTIC_PRUNE_KEEP_LAST'],
-        '--keep-hourly', os.environ['RESTIC_PRUNE_KEEP_HOURLY'],
-        '--keep-daily', os.environ['RESTIC_PRUNE_KEEP_DAILY'],
-        '--keep-weekly', os.environ['RESTIC_PRUNE_KEEP_WEEKLY'],
-        '--keep-monthly', os.environ['RESTIC_PRUNE_KEEP_MONTHLY'],
-        '--host', hostname(),
-        '--cleanup-cache'
-    ]
-
-def make_check_command(read_data=None):
-    cmd = ['check']
-    if read_data:
-        cmd += ['--read-data-subset', f'{read_data}/7']
-    return cmd
+def call_combo(args, calls):
+    for call in calls:
+        code = call_restic(args, call)
+        if code != 0:
+            return code
+    return 0
 
 def call_restic(args, mode=None):
     repo = ['-r', repo_path()]
@@ -72,22 +45,12 @@ def call_restic(args, mode=None):
         print('ERROR: Check failed!')
     return code
 
-def call_combo(args, calls):
-    for call in calls:
-        code = call_restic(args, call)
-        if code != 0:
-            return code
-    return 0
+def exit_if_restic_already_running():
+    if 'restic' in [i.name() for i in psutil.process_iter(['name'])]:
+        exit('A restic process is already running. Exiting.')
 
 def hostname():
     return run(['hostname'], capture_output=True, check=True, text=True).stdout.strip()
-
-def repo_path():
-    service = os.getenv('RESTIC_CONFIG_REMOTE_SERVICE')
-    if service == 'wasabi':
-        return f's3:https://{os.getenv("WASABI_SERVICE_URL")}/{os.getenv("WASABI_BUCKET_NAME")}'
-    else:
-        raise NotImplementedError(f'Unsupported remote service: {service}')
 
 def load_environment():
     basedir = os.path.dirname(__file__)
@@ -97,20 +60,6 @@ def load_environment():
         shutil.copyfile(os.path.join(basedir, 'env_base.txt'), env_file)
         exit(10)
     load_dotenv()
-
-def exit_if_restic_already_running():
-    if 'restic' in [i.name() for i in psutil.process_iter(['name'])]:
-        exit('A restic process is already running. Exiting.')
-
-def parse_args():
-    args = sys.argv
-    switch = None
-    if len(args) == 1:
-        return (switch, [])
-    elif args[1] in implemented_commands or args[1] in implemented_combo_commands:
-        switch = args[1]
-        del args[1]
-    return (switch, args[1:])
 
 def main():
     exit_if_restic_already_running()
@@ -128,7 +77,57 @@ def main():
         return call_restic(args)
     else:
         raise NotImplementedError()
-    return 0
+
+def make_backup_command():
+    try:
+        include = ['--files-from', os.environ['RESTIC_INCLUDES_FILE']]
+        if not os.path.isfile(include[1]):
+                raise FileNotFoundError('Includes file configured but not found')
+    except KeyError:
+        include = []
+    try:
+        exclude = ['--exclude-file', os.environ['RESTIC_EXCLUDES_FILE']]
+        if not os.path.isfile(exclude[1]):
+                raise FileNotFoundError('Excludes file configured but not found')
+    except KeyError:
+        exclude = []
+    return ['backup'] + include + exclude
+
+def make_check_command(read_data=None):
+    cmd = ['check']
+    if read_data:
+        cmd += ['--read-data-subset', f'{read_data}/7']
+    return cmd
+
+def make_prune_command():
+    return [
+        'forget',
+        '--prune',
+        '--keep-last', os.environ['RESTIC_PRUNE_KEEP_LAST'],
+        '--keep-hourly', os.environ['RESTIC_PRUNE_KEEP_HOURLY'],
+        '--keep-daily', os.environ['RESTIC_PRUNE_KEEP_DAILY'],
+        '--keep-weekly', os.environ['RESTIC_PRUNE_KEEP_WEEKLY'],
+        '--keep-monthly', os.environ['RESTIC_PRUNE_KEEP_MONTHLY'],
+        '--host', hostname(),
+        '--cleanup-cache'
+    ]
+
+def parse_args():
+    args = sys.argv
+    switch = None
+    if len(args) == 1:
+        return (switch, [])
+    elif args[1] in implemented_commands or args[1] in implemented_combo_commands:
+        switch = args[1]
+        del args[1]
+    return (switch, args[1:])
+
+def repo_path():
+    service = os.getenv('RESTIC_CONFIG_REMOTE_SERVICE')
+    if service == 'wasabi':
+        return f's3:https://{os.getenv("WASABI_SERVICE_URL")}/{os.getenv("WASABI_BUCKET_NAME")}'
+    else:
+        raise NotImplementedError(f'Unsupported remote service: {service}')
 
 if __name__ == '__main__':
     try:
